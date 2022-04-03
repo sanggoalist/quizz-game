a6fdc3842a8537fb17e6c782aeec847721db1d33
sanggoalist <s.tran@goalist.co.jp>
2022-04-03 20:53:13 +0700

    change to quiz

--- pages/api/questions/index.ts
+++ pages/api/questions/index.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { QuestionResponse } from '../../../libs/type'
-import questions from "../../../dataset/quizz-question.json";
+import questions from "../../../dataset/quiz-question.json";
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<QuestionResponse>
--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 import { Button, Card, Radio, Result, Space } from "antd";
 import { CaretLeftOutlined, CheckOutlined, LeftOutlined, RightOutlined, SmileOutlined } from "@ant-design/icons";
 
-const QuizzPage: NextPage = () => {
+const QuizPage: NextPage = () => {
 
     const handleSelectAnwser = (event: any) => {
 
@@ -27,7 +27,7 @@ const QuizzPage: NextPage = () => {
     return <div className={style.layout}>
         <div className= {style.wrapper}>
             <div className={style.container}>
-                <h1>Quizz Game</h1>
+                <h1>Quiz Game</h1>
                 <p>1/<b>12</b></p>
                 <div className={style.content}>
                     <p><b>Q.1: </b>This is a question ?</p>
@@ -83,4 +83,4 @@ const QuizzPage: NextPage = () => {
     </div>
 }
 
-export default QuizzPage;
+export default QuizPage;
